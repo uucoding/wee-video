417a3ef65c05c9eabda78c646938c6ca26808bde
wuy <1311695042@qq.com>
2019-07-14 17:13:37 +0800

    +增加个人中心

---
+++ wee-video-miniapp/pages/me/me.js
@@ -0,0 +1,30 @@
+//index.js
+//获取应用实例
+const app = getApp()
+const { $ } = require("../utils/request.js")
+const { toastSuccess} = require("../utils/toast.js")
+
+Page({
+  data: {
+    //tab选项
+    tabs:['作品', '喜欢'],
+    // 默认选中的tab下标
+    tabActiveIndex: 0,
+    user: {
+      nickname: 'weecoding',
+      faceImage: '../resource/images/face-avator.png',
+      likeCount: 20,
+      followCount: 33,
+      fansCount: 56,
+    }
+  },
+
+  /**
+   *  切换table选项卡
+   */
+  triggerTab: function(e) {
+    this.setData({
+      tabActiveIndex: e.currentTarget.dataset.index
+    }) 
+  }
+})
